5228c75ef7a494a2a02ed99db9dfe9aa04de2b41
Kstrella <90272570+Kstrella@users.noreply.github.com>
2024-04-22 14:04:41 -0400

    Added front end pages
    Imported covid long term, price changes, and query 3

--- app/query3/page.js
+++ app/query3/page.js
@@ -1,49 +1,20 @@
-import { Header } from "@/app\components\Header.js";
-
-export function Search() {
-    return (
-        <div className="bg-white">
-            <label htmlFor="start-date" className="sr-only">
-                Date
-            </label>
-            <label htmlFor="start-date" className="sr-only">
-                Date
-            </label>
-            <input
-                type="date"
-                name="start-date"
-                id="date"
-                className="block w-half rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
-                placeholder="end date"
-            />
-            <details className="dropdown">
-                <summary className="m-1 btn text-gray-900 ring-gray-300 focus:ring-2 focus:ring-inset">Airline</summary>
-                <ul className="block w-half rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 sm:text-sm sm:leading-6">
-                    <li><a>Southwest</a></li>
-                    <li><a>Jetblue</a></li>
-                </ul>
-            </details>
-        </div>
-
-
-    )
-}
-
+import { AirlineDropdown } from '@/app\components\AirlineDropdown.js';
 export default function Home() {
+
     return (
         <div className="bg-white">
-            <Header/>
+
             <div className="relative isolate pt-14">
                 <div className="py-24 sm:py-32 lg:pb-40">
                     <div className="mx-auto max-w-7xl px-6 lg:px-8">
                         <div className="mx-auto max-w-2xl text-center">
                             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                                Cancellations Since Covid
+                                Stock Price Influences By Diseases
                             </h1>
                         </div>
-                        <div className="mt-10 flex items-center justify-center gap-x-6">
-                            <Search/>
 
+                        <div className="mt-10 flex items-center justify-center gap-x-6">
+                            <AirlineDropdown/>
                             <button
                                 className="rounded-md bg-gray-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">
                                 Submit <span aria-hidden="true">→</span>
